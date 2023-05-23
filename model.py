b2b1e9a9709a5b6b57611cce1ff9107832bc4579
Chen Songsong <17862708589@163.com>
2023-05-23 21:31:00 +0800

    share parameter

--- model.py
+++ model.py
@@ -28,100 +28,6 @@ class Model(collections.namedtuple("Model", ["model_name",
 
 # recall
 class DSSM(Model):
-    class DNN(Layer):
-        """The Multi Layer Percetron
-          Input shape
-            - nD tensor with shape: ``(batch_size, ..., input_dim)``. The most common situation would be a 2D input with
-            shape ``(batch_size, input_dim)``.
-          Output shape
-            - nD tensor with shape: ``(batch_size, ..., hidden_size[-1])``. For instance, for a 2D input with shape
-            ``(batch_size, input_dim)``, the output would have shape ``(batch_size, hidden_size[-1])``.
-          Arguments
-            - **hidden_units**:list of positive integer, the layer number and units in each layer.
-            - **activation**: Activation function to use.
-            - **l2_reg**: float between 0 and 1. L2 regularizer strength applied to the kernel weights matrix.
-            - **dropout_rate**: float in [0,1). Fraction of the units to dropout.
-            - **use_bn**: bool. Whether use BatchNormalization before activation or not.
-            - **output_activation**: Activation function to use in the last layer.If ``None``,it will be same as ``activation``.
-            - **seed**: A Python integer to use as random seed.
-        """
-        def __init__(self, hidden_units, activation='relu', l2_reg=0, dropout_rate=0, use_bn=False,
-                     output_activation=None,
-                     seed=1024, **kwargs):
-            self.hidden_units = hidden_units
-            self.activation = activation
-            self.l2_reg = l2_reg
-            self.dropout_rate = dropout_rate
-            self.use_bn = use_bn
-            self.output_activation = output_activation
-            self.seed = seed
-
-            super(DNN, self).__init__(**kwargs)
-
-        def build(self, input_shape):
-            # if len(self.hidden_units) == 0:
-            #     raise ValueError("hidden_units is empty")
-            input_size = input_shape[-1]
-            hidden_units = [int(input_size)] + list(self.hidden_units)
-            self.kernels = [self.add_weight(name='kernel' + str(i),
-                                            shape=(
-                                                hidden_units[i], hidden_units[i + 1]),
-                                            initializer=glorot_normal(
-                                                seed=self.seed),
-                                            regularizer=l2(self.l2_reg),
-                                            trainable=True) for i in range(len(self.hidden_units))]
-            self.bias = [self.add_weight(name='bias' + str(i),
-                                         shape=(self.hidden_units[i],),
-                                         initializer=Zeros(),
-                                         trainable=True) for i in range(len(self.hidden_units))]
-            if self.use_bn:
-                self.bn_layers = [BatchNormalization() for _ in range(len(self.hidden_units))]
-
-            self.dropout_layers = [Dropout(self.dropout_rate, seed=self.seed + i) for i in
-                                   range(len(self.hidden_units))]
-
-            self.activation_layers = [activation_layer(self.activation) for _ in range(len(self.hidden_units))]
-
-            if self.output_activation:
-                self.activation_layers[-1] = activation_layer(self.output_activation)
-
-            super(DNN, self).build(input_shape)  # Be sure to call this somewhere!
-
-        def call(self, inputs, training=None, **kwargs):
-
-            deep_input = inputs
-
-            for i in range(len(self.hidden_units)):
-                fc = tf.nn.bias_add(tf.tensordot(
-                    deep_input, self.kernels[i], axes=(-1, 0)), self.bias[i])
-
-                if self.use_bn:
-                    fc = self.bn_layers[i](fc, training=training)
-                try:
-                    fc = self.activation_layers[i](fc, training=training)
-                except TypeError as e:  # TypeError: call() got an unexpected keyword argument 'training'
-                    print("make sure the activation function use training flag properly", e)
-                    fc = self.activation_layers[i](fc)
-
-                fc = self.dropout_layers[i](fc, training=training)
-                deep_input = fc
-
-            return deep_input
-
-        def compute_output_shape(self, input_shape):
-            if len(self.hidden_units) > 0:
-                shape = input_shape[:-1] + (self.hidden_units[-1],)
-            else:
-                shape = input_shape
-
-            return tuple(shape)
-
-        def get_config(self, ):
-            config = {'activation': self.activation, 'hidden_units': self.hidden_units,
-                      'l2_reg': self.l2_reg, 'use_bn': self.use_bn, 'dropout_rate': self.dropout_rate,
-                      'output_activation': self.output_activation, 'seed': self.seed}
-            base_config = super(DNN, self).get_config()
-            return dict(list(base_config.items()) + list(config.items()))
 
     def get_model_fn(self):
 
@@ -255,17 +161,28 @@ class DSSM_Two_Pair_Loss(Model):
             item_embeddings = []
             feature_square_embeddings = []
 
+            # item and ite_neg share embedding
+            pid_vocab_size = 100
+            item_neg = tf.compat.v1.string_to_hash_bucket_fast(tf.as_string(features["item_neg"]), pid_vocab_size)
+            item = tf.compat.v1.string_to_hash_bucket_fast(tf.as_string(features["item"]), pid_vocab_size)
+
+            embedding_size = 16
+            embeddings = tf.compat.v1.get_variable(name="embeddings", dtype=tf.float32,
+                                                   shape=[pid_vocab_size, embedding_size])
+
+            item_neg_emb = tf.nn.embedding_lookup(embeddings, item_neg)
+            item_emb = tf.nn.embedding_lookup(embeddings, item)
+
+            dnn_layer = DNN(hidden_units=params['hidden_units'], activation='relu', use_bn=True)
+
+            item_output = dnn_layer(item_emb)
+            item_neg_output = dnn_layer(item_neg_emb)
+
             for feature in ['uid', 'gender', 'bal']:
                 feature_emb = tf.compat.v1.feature_column.input_layer(features, params['feature_columns'][feature])
                 feature_embeddings.append(feature_emb)
 
             dnn_user_input = tf.concat(feature_embeddings, axis=1, name='user')
-
-            neg_item_dnn_input = tf.compat.v1.feature_column.input_layer(features,
-                                                                         params['feature_columns']['item_neg'])
-
-            pos_item_dnn_input = tf.compat.v1.feature_column.input_layer(features, params['feature_columns']['item'])
-            item_net = tf.concat(item_embeddings, axis=1, name='item')
 
             for unit in params['hidden_units']:
                 dnn_user_input = tf.compat.v1.layers.dense(dnn_user_input, units=unit, activation=tf.nn.relu)
@@ -273,26 +190,15 @@ class DSSM_Two_Pair_Loss(Model):
                 dnn_user_input = tf.compat.v1.layers.dropout(dnn_user_input)
                 dnn_user_input = tf.nn.l2_normalize(dnn_user_input)
 
-                # 正负样本的item需要share 参数
-                neg_item_dnn_input = tf.compat.v1.layers.dense(neg_item_dnn_input, units=unit, activation=tf.nn.relu)
-                neg_item_dnn_input = tf.compat.v1.layers.batch_normalization(neg_item_dnn_input)
-                neg_item_dnn_input = tf.compat.v1.layers.dropout(neg_item_dnn_input)
-                neg_item_dnn_input = tf.nn.l2_normalize(neg_item_dnn_input)
-
-                pos_item_dnn_input = tf.compat.v1.layers.dense(pos_item_dnn_input, units=unit, activation=tf.nn.relu)
-                pos_item_dnn_input = tf.compat.v1.layers.batch_normalization(pos_item_dnn_input)
-                pos_item_dnn_input = tf.compat.v1.layers.dropout(pos_item_dnn_input)
-                pos_item_dnn_input = tf.nn.l2_normalize(pos_item_dnn_input)
-
-            dot_1 = tf.reduce_sum(tf.multiply(dnn_user_input, pos_item_dnn_input), axis=1, keepdims=True) / float(
+            dot_1 = tf.reduce_sum(tf.multiply(dnn_user_input, item_output), axis=1, keepdims=True) / float(
                 params['temperature'])
 
-            dot_2 = tf.reduce_sum(tf.multiply(dnn_user_input, neg_item_dnn_input), axis=1, keepdims=True) / float(
+            dot_2 = tf.reduce_sum(tf.multiply(dnn_user_input, item_neg_output), axis=1, keepdims=True) / float(
                 params['temperature'])
 
             if mode == tf.estimator.ModeKeys.PREDICT:
                 predictions = {"user_emb": dnn_user_input,
-                               "item_embedding": pos_item_dnn_input}
+                               "item_embedding": item_output}
                 return tf.estimator.EstimatorSpec(mode, predictions=predictions)
 
             loss = tf.maximum(0, dot_1 - dot_2 + params['margin'])
